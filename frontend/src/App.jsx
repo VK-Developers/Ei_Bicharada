5fa48dd3059d0b86e6006003501c0bd5b6c2a271
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-08-02 13:18:08 -0300

    feat: config

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, } from 'react-native';
+
+
+function App() {
+  return (
+    <View>
+      <Text>APP</Text>
+    </View>
+  );
+}
+
+export default App;
