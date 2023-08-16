beee8b01efd6756dc6deea41d5ed07a91e090904
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-08-15 19:20:27 -0700

    feat: new backgrounds

--- frontend/src/localized/images.jsx
+++ frontend/src/localized/images.jsx
@@ -3,6 +3,8 @@ export default {
     one: require('../assets/bg_01.png'),
     two: require('../assets/bg_02.png'),
     tree: require('../assets/bg_03.png'),
+    four: require('../assets/bg_04.png'),
+    five: require('../assets/bg_05.png'),
   },
   donate: require('../assets/donate.png'),
   logo: require('../assets/logo.png'),
