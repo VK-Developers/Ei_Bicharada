81158f68f8088ce3919252c73c1df7202b1b9552
Kaioeduduardo <kaioedsago@gmail.com>
2023-10-05 18:00:54 -0300

    Feat: Fix navbar

--- dashboard/src/structure/images.jsx
+++ dashboard/src/structure/images.jsx
@@ -1,7 +1,7 @@
 import Denuncia from '../assets/imgs/icons/denuncia.png';
 import Resgate from '../assets/imgs/icons/resgate.png';
 import Eventos from '../assets/imgs/icons/evento.png';
-import Noticias from '../assets/imgs/icons/denuncia.png';
+import Noticias from '../assets/imgs/icons/noticia.png';
 import LarTemporario from '../assets/imgs/icons/larTemporario.png';
 
 export default {
