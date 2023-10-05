eed4052c822199a4331777aba53914a0e48acdd2
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-10-05 00:42:47 -0300

    feat: back to normal

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const functions = require("firebase-functions");
 const app = require('./api');
 
 const PORT = process.env.API_PORT;
@@ -7,5 +6,3 @@ const PORT = process.env.API_PORT;
 app.get('/', (_request, response) => response.send('Hey Pet! API'));
 
 app.listen(PORT, () => console.log("API running on " + PORT));
-
-exports.api = functions.https.onRequest(app);
