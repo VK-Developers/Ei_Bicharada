1a9181195a354a18465785560ef698ff5b65a1c2
Kaioedsago <kaioedsago@gmail.com>
2023-10-17 15:00:09 -0300

    Feat: Input file ok

--- dashboard/src/components/content/Resgate.jsx
+++ dashboard/src/components/content/Resgate.jsx
@@ -46,13 +46,40 @@ const Resgate = () => {
             rows="5"
             cols="30"
             placeholder="Ocorrido"
-            className="w-3/12 h-[200px] rounded-md shadow-sm p-1 shadow-black border-black border-[0.20px]"
+            className="w-3/12 min-h-[250px] max-h-[300px] rounded-md shadow-sm p-1 shadow-black border-black border-[0.20px]"
           ></textarea>
-          <div className="max-w-[400px] p-1 min-h-[200px] bg-blue-800">
-          <label>
-            <input type="file" accept="image/png/jpg" id="imgAnimal" />
-        </label>
-        <img src="imgAnimal" className="w-96" alt="" />
+
+          <div class="ml-4 w-3/12 max-w-[468px] h-[260px]">
+            <label
+              for="dropzone-file"
+              class="flex flex-col items-center justify-center h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span class="font-semibold">Click to upload</span> or drag and
+                  drop
+                </p>
+                <p class="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG 
+                </p>
+              </div>
+              <input id="dropzone-file" type="file" class="hidden" />
+            </label>
           </div>
         </div>
       </div>
