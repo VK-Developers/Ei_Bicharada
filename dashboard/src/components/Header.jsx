c50cd10b8747ba777ec9247779db35c847417466
Kaioedsago <Kaioedsago@gmail.com>
2023-10-18 18:58:11 -0300

    Feat: Header buttons created

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -1,16 +1,18 @@
 const Header = () => {
-
-    return(
-        <>
-        <div className="flex relative shadow-sm shadow-black bg-[#7c97e7] w-full h-12">
-        <div>
-            soon
+  return (
+    <>
+      <div className="flex relative items-center px-5 justify-between shadow-sm shadow-black bg-[#7c97e7] w-full h-12">
+        <div className="left-0 ">
+          <span className="text-2xl">Nome do Content</span>
         </div>
-
+        <div className="flex gap-10">
+          <button className="hover:text-white hover:font-semibold cursor-pointer" >Adicionar</button>
+          <button className="hover:text-white hover:font-semibold cursor-pointer" >Novos</button>
+          <button className="hover:text-white hover:font-semibold cursor-pointer" >Lista</button>
         </div>
-        
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Header;
