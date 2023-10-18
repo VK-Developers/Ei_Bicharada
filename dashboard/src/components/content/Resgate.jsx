ecf6e09ba3bdf2406c8cd9be581f2260fce21fdd
Kaioedsago <Kaioedsago@gmail.com>
2023-10-18 14:06:47 -0300

    Feat: State image ok

--- dashboard/src/components/content/Resgate.jsx
+++ dashboard/src/components/content/Resgate.jsx
@@ -1,4 +1,22 @@
+import React, { useState } from 'react';
+
 const Resgate = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      setImageUrl(event.target.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  
   return (
     <>
       <div className="flex-col flex">
@@ -49,38 +67,35 @@ const Resgate = () => {
             className="w-3/12 min-h-[250px] max-h-[300px] rounded-md shadow-sm p-1 shadow-black border-black border-[0.20px]"
           ></textarea>
 
-          <div class="ml-4 w-3/12 max-w-[468px] h-[260px]">
-            <label
-              for="dropzone-file"
-              class="flex flex-col items-center justify-center h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-            >
-              <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg
-                  class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                  />
-                </svg>
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span class="font-semibold">Click to upload</span> or drag and
-                  drop
-                </p>
-                <p class="text-xs text-gray-500 dark:text-gray-400">
-                  SVG, PNG, JPG 
-                </p>
-              </div>
-              <input id="dropzone-file" type="file" class="hidden" />
-            </label>
+<div className="ml-4 w-3/12 max-w-[468px] h-[260px]">
+      <label
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          objectFit: 'fill',
+        }}
+      >
+        {!imageUrl && (
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG</p>
           </div>
+        )}
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleImageUpload}
+        />
+      </label>
+    </div>
         </div>
       </div>
     </>
