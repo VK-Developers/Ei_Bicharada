66229b94351eb295a929bf349b07a1441e792ac2
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-10-22 22:05:15 -0300

    feat: prestacao de contas OK

--- app/src/screens/stack/PrestacaoDeContas.jsx
+++ app/src/screens/stack/PrestacaoDeContas.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import PDF from 'react-native-pdf';
+import { WebView } from 'react-native-webview';
 //Components
 import Footer from '../../component/Footer';
 import Header from '../../component/Header';
@@ -8,6 +8,9 @@ import Background from '../../component/Background';
 import ToggleMenu from '../../component/ToggleMenu';
 
 function PrestacaoDeContas({route: { params }}) {
+  const googleView = 'https://docs.google.com/gview?embedded=true&url=';
+  const url = 'https://atespets.com.br/accountabilities/template.pdf'
+
   return (
     <>
       <Background img={'tree'} />
@@ -15,32 +18,11 @@ function PrestacaoDeContas({route: { params }}) {
         <View style={styles.container}>
           <View style={{ flexGrow: 1 }}>
             <Header name={params.name} />
-            <View style={{ flexGrow: 1, backgroundColor: 'green' }}>
-{/* 
-            <PDF
-              source={{ uri: 'https://atespets.com.br/accountabilities/template.pdf', cache: true, headers: {Pragma:'public'} }}
-              onLoad={() => console.log('PDF carregado com sucesso')}
-              onError={(error) => console.log('Erro ao carregar o PDF: ', error)}
-              style={styles.pdf}
-            /> */}
-
-            {/* <PDF
-              source={{ uri: 'https://atespets.com.br/accountabilities/template.pdf' }}
-              onLoadComplete={(numberOfPages,filePath) => {
-                  console.log(`Number of pages: ${numberOfPages}`);
-              }}
-              onPageChanged={(page,numberOfPages) => {
-                  console.log(`Current page: ${page}`);
-              }}
-              onError={(error) => {
-                  console.log(error);
-              }}
-              onPressLink={(uri) => {
-                  console.log(`Link pressed: ${uri}`);
-              }}
-              // style={styles.pdf}
-            /> */}
-
+            <View style={{ flexGrow: 1 }}>
+                <WebView
+                  source={{ uri: googleView + url }}
+                  style={{flexGrow: 1}}
+                />
             </View>
           </View>
           <Footer />
