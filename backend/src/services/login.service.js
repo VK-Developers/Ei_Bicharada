0f2919825305ffd60e96a6bd46f3e836b3719b79
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-10-26 13:30:09 -0300

    feat: login log (OK)

--- backend/src/services/login.service.js
+++ backend/src/services/login.service.js
@@ -7,6 +7,18 @@ const reject = {
   msg: 'senha ou email invalidos'
 }
 
+const addToLogTable = async (data) => {
+  const {name, phone} = data;
+
+  const obj = {
+    name,
+    phone,
+    acessed: new Date()
+  }
+
+  await models.loginLog.create(obj);
+}
+
 module.exports = async (data) => {
   const { password, email } = await validation(data);
 
@@ -15,6 +27,9 @@ module.exports = async (data) => {
   if (!!founded) {
     if (founded.password === password) {
       const userData = await models.user.findOne({ where: { email } });
+
+      await addToLogTable(userData)
+
       const token = createToken(userData);
       return { status: 200, token }
     };
