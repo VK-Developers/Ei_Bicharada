079ec11d23235d771a67f54258162320be959c72
Vincenzo F. Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2023-11-02 15:56:46 -0300

    fix: login with 6 char

--- backend/src/validations/schemas/login.schema.js
+++ backend/src/validations/schemas/login.schema.js
@@ -3,6 +3,6 @@ const Joi = require('joi');
 module.exports = {
     create: Joi.object({
         email: Joi.string().email().required(),
-        password: Joi.string().min(8).required(),
+        password: Joi.string().min(6).required(),
     }),
 }
