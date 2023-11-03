7d32c3bc164d0cd51bff52efb11e111d8eb45b3d
Vincenzo F. Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2023-11-03 09:53:48 -0300

    feat: Adoption (O-O)

--- backend/src/services/adoption.service.js
+++ backend/src/services/adoption.service.js
@@ -4,21 +4,25 @@ const { authentication } = require('../tools');
 
 module.exports = {
     getAll: async (accepted, token) => {
-        const { region } = authentication.verifyToken(token);
+        // const { region } = authentication.verifyToken(token);
         
-        if (accepted === undefined || region === 'all') {
-            const result = await models.adoption.findAll();
-            return {status: 200, result};
-        }
+        // if (accepted === undefined || region === 'all') {
+        //     const result = await models.adoption.findAll();
+        //     return {status: 200, result};
+        // }
         
-        const result = await models.adoption.findAll({
-            where: {
-                region,
-                new: !JSON.parse(accepted)
-              }
-        });
+        // const result = await models.adoption.findAll({
+        //     where: {
+        //         region,
+        //         new: !JSON.parse(accepted)
+        //       }
+        // });
 
+        // return {status: 200, result};
+        
+        const result = await models.adoption.findAll();
         return {status: 200, result};
+
     },
     getById: async (id) => {
         const result = await models.adoption.findByPk(id);
