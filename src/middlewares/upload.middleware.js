8da6b0b4ebae4c62840331032e6c93b72928ceed
Vincenzofdg <79121466+Vincenzofdg@users.noreply.github.com>
2024-05-18 07:39:15 +0000

    Deploying to backend_vps from @ Vincenzofdg/Hey_Pet@0b1699ea4f57ab60c5d035a82b382d9345304588 🚀

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,18 +1,49 @@
+// const multer = require('multer');
+// const path = require('path')
+
+// const config = multer.diskStorage({
+//   destination: (request, _file, callback) => {
+//     const SplitFullUrl = (request.baseUrl).split('/')
+//     const local = '../../uploads' + request.baseUrl
+//     const destination = path.join(__dirname, local)
+//     callback(null, destination);
+//   },
+//   filename: (_request, file, callback) => {
+//     const baseName = file.originalname;
+//     return callback(null, baseName);
+//   }
+// })
+
+// const upload = multer({ storage: config });
+
+// module.exports = upload;
+
+
 const multer = require('multer');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+
+// Verifica e cria o diretório de destino, se não existir
+const checkAndCreateUploadsFolder = (folderPath) => {
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+}
 
 const config = multer.diskStorage({
-  destination: (request, _file, callback) => {
-    const local = '../../uploads' + request.baseUrl
-    const destination = path.join(__dirname, local)
-    callback(null, destination);
+  destination: (req, _file, callback) => {
+    const SplitFullUrl = (req.baseUrl).split('/')
+    const local = path.join(__dirname, '../../uploads/', SplitFullUrl[2]);
+    checkAndCreateUploadsFolder(local); // Verifica e cria o diretório de destino
+    callback(null, local);
   },
-  filename: (_request, file, callback) => {
-    const baseName = file.originalname;
-    return callback(null, baseName);
+  filename: (_req, file, callback) => {
+    const baseName = file.originalname; // Adiciona um timestamp ao nome do arquivo
+    callback(null, baseName);
   }
-})
+});
 
 const upload = multer({ storage: config });
 
 module.exports = upload;
+
